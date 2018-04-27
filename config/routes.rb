2db90d3d465ac092d05767d4bd365e1c75452af3
Yomaira Escano <yjescano@gmail.com>
2018-04-27 21:11:21 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :products, only: [:index, :add]
-
-  get '/', to: 'products#index'
+  
+  root 'products#index'
+  post '/', to: 'products#add'
   # get 'add',
 
   # The priority is based upon order of creation: first created -> highest priority.
